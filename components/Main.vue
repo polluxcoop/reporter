ebbf384cfdb56a4ee2d8331aff9eacf0d1dab842
JulianVilloria <julian@polluxcoop.com>
2022-09-16 17:13:19 -0300

    add forms

--- components/Main.vue
+++ components/Main.vue
@@ -1,8 +1,31 @@
 <template>
-  <h1>testing</h1>
+  <div v-if="step === 0">
+    <form>
+      <textarea v-model="details" placeholder="Detalles del reporte" />
+      <button type="button" @click="step++">Generar</button>
+    </form>
+  </div>
+  <div v-else-if="step === 1">
+    <form>
+      <input v-model="signature" placeholder="Clave publica" />
+      <button type="button" @click="step++">Nuevo</button>
+      <button type="button" @click="step--">back</button>
+    </form>
+  </div>
+  <div v-else-if="step === 2">
+    <p>{{ details }}</p>
+    <button type="button" @click="step--">back</button>
+  </div>
 </template>
 <script>
 export default {
   name: "Main",
+  data() {
+    return {
+      details: "",
+      signature: "",
+      step: 0,
+    };
+  },
 };
 </script>
