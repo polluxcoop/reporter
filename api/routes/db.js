cacf2601ea3a8ccd11c73ec652394b6e55c0e698
daniel lenarduzzi <daniel.lenarduzzi@leniolabs.com>
2022-09-16 19:48:09 -0300

    feat: create report

--- api/routes/db.js
+++ api/routes/db.js
@@ -14,18 +14,6 @@ db.serialize(function () {
     "CREATE TABLE nonces (signature NVARCHAR(132) PRIMARY KEY NOT NULL, nonce INTEGER NOT NULL)"
   );
 
-  const stmt = db.prepare(
-    "INSERT INTO reports (signature, description, title) VALUES (?,?,?)"
-  );
-
-  stmt.run(["lorem", "hola", "mundo"]);
-
-  stmt.finalize();
-
-  db.each("SELECT * FROM reports", function (err, row) {
-    console.log(row.signature + ": " + row.description);
-  });
-
   const router = Router();
 
   router.use(express.json());
@@ -34,7 +22,47 @@ db.serialize(function () {
     const sql = `SELECT * FROM reports where signature = '${req.params.signature}'`;
 
     db.get(sql, function (err, row) {
+      if (err) {
+        return res.status(404).json({ err });
+      }
+
       res.json(row);
+    });
+  });
+
+  router.post("/reports", function (req, res, next) {
+    const { nonceSignature, reportSignature, title, description } = req.body;
+
+    const reportInsert = db.prepare(
+      "INSERT INTO reports (signature, description, title) VALUES (?,?,?)"
+    );
+
+    reportInsert.run([reportSignature, description, title]);
+
+    reportInsert.finalize();
+
+    const selectNonce = `SELECT * FROM nonces where signature = '${nonceSignature}'`;
+
+    db.get(selectNonce, function (err, row) {
+      if (row) {
+        const noncesUpdate = db.prepare(
+          "UPDATE nonces SET nonce = ? WHERE signature = ?"
+        );
+
+        noncesUpdate.run([row.nonce + 1, nonceSignature]);
+
+        noncesUpdate.finalize();
+      } else {
+        const noncesInsert = db.prepare(
+          "INSERT INTO nonces (signature, nonce) VALUES(?,?)"
+        );
+
+        noncesInsert.run([nonceSignature, 1]);
+
+        noncesInsert.finalize();
+      }
+
+      return res.json({ row, err });
     });
   });
 
@@ -42,7 +70,11 @@ db.serialize(function () {
     const sql = `SELECT * FROM nonces where signature = '${req.params.signature}'`;
 
     db.get(sql, function (err, row) {
-      res.json(row);
+      if (err) {
+        return res.json({ nonce: 0 });
+      }
+
+      res.json({ nonce: row.nonce });
     });
   });
 
